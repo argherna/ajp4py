cce23bdb7c223a12c1f584095f9867114a8ad93a
Andy Gherna <argherna@gmail.com>
2019-05-23 16:51:54 -0500

    Refactor and document
    - Add params as a parameter to get
    
    - Add params as a parameter to request
    
    - Change request to use AjpConnection for data exchange
    
    - Add documentation string for request and get

--- ajp4py/api.py
+++ ajp4py/api.py
@@ -7,40 +7,60 @@ Implements the API for sending AJP requests.
 
 from urllib.parse import urlparse
 
-from . import AJP4PY_LOGGER
-from .ajp_types import DEFAULT_AJP_SERVER_PORT, AjpCommand, AjpHeader
-from .models import AjpForwardRequest
-from .protocol import connect, disconnect, send_and_receive
+from .ajp_types import DEFAULT_AJP_SERVER_PORT, AjpCommand
+from .models import AjpForwardRequest, AjpAttribute
+from .protocol import AjpConnection
 
 
-def request(ajp_cmd, url, request_headers=None,
+def request(ajp_cmd, url, params=None, headers=None,
             attributes=None):
-    '''
+    r'''
     Send a generic request to the servlet container.
+
+    :type ajp_cmd: An AjpCommand (equiv. to HTTP method)
+    :param url: Url in the form `ajp://host[:port][/path]`
+    :param params: (optional) Dictionary of parameters that will
+        be converted into the query string.
+    :param headers: (optional) Dictionary of request headers
+    :param attributes: (optional) Dictionary of request attributes
+    :return: :class:`AjpResponse <AjpResponse>` object
+    :rtype: ajp4py.AjpResponse
     '''
     parsed_url = urlparse(url)
     host_name = parsed_url.netloc.split(':')[0]
-
     port = parsed_url.port or DEFAULT_AJP_SERVER_PORT
+
+    if params:
+        q_params = ['='.join([nm, val]) for nm, val in params.items()]
+        if not attributes:
+            attributes = {}
+        attributes[AjpAttribute.QUERY_STRING] = '&'.join(q_params)
 
     ajp_req = AjpForwardRequest(method=ajp_cmd,
                                 req_uri=parsed_url.path,
                                 remote_host=host_name,
-                                request_headers=request_headers or {},
+                                request_headers=headers or {},
                                 attributes=attributes or {})
 
-    ajp_conn = connect(host_name, port)
-    ajp_resp = send_and_receive(ajp_conn, ajp_req)
-    disconnect(ajp_conn)
+    with AjpConnection(host_name, port) as ajp_conn:
+        ajp_resp = ajp_conn.send_and_receive(ajp_req)
 
     return ajp_resp
 
 
-def get(url, **kwargs):
-    '''
+def get(url, params=None, **kwargs):
+    r'''
     Sends a GET command to the servlet container.
+
+    :param url: Url in the form `ajp://host[:port][/path]`
+    :param params: (optional) Dictionary of parameters that will
+        be converted into the query string.
+    :param headers: (optional) Dictionary of request headers
+    :param attributes: (optional) Dictionary of request attributes
+    :return: :class:`AjpResponse <AjpResponse>` object
+    :rtype: ajp4py.AjpResponse
     '''
-    return request(AjpCommand.GET, url, **kwargs)
+    return request(AjpCommand.GET, url, params=params, **kwargs)
 
 
 def post():
