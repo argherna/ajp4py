32ce0beb7af5302cdc3087e06c665ce71c8d922b
Andy Gherna <argherna@gmail.com>
2019-05-23 16:43:31 -0500

    Encapsulate connection into class
    AjpConnection replaces the separate connect/send_and_receive/disconnect
    by adding __enter__ and __exit__ to let the with keyword be used for
    connections. This helps ensure resources are closed at the end of use.

--- ajp4py/protocol.py
+++ ajp4py/protocol.py
@@ -5,9 +5,6 @@ protocol.py
 Manages communications between the servlet container and this
 library.
 
-This is a low-level module and should be used by the other
-modules in this library. However, to have more explicit control
-over interactions, it can be used by clients.
 '''
 
 import socket
@@ -18,45 +15,49 @@ from . import PROTOCOL_LOGGER
 from .models import AjpResponse
 
 
-def connect(host_name, port):
+class AjpConnection:
+    r'''Encapsulates a connection to a servlet container.
+
+    Use the `with` construct to use an instance of AjpConnection. This 
+    will guarantee connections are closed at the end.
+
+    ..code-block :: Python
+
+        with AjpConnection('localhost', 8009) as ajp_conn:
+            ajp_response = ajp_conn.send_and_receive(ajp_request)
+
     '''
-    Returns a socket for communicating to a servlet container.
+    def __init__(self, host_name, port):
+        self._host_name = host_name
+        self._port = port
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    :param host_name: name of the host to connect to.
-    :param port: port to connect to.
-    '''
-    skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    skt.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    skt.connect((host_name, port))
-    PROTOCOL_LOGGER.debug('Connection established:%s', skt)
-    return skt
+    def __enter__(self):
+        self.connect()
+        return self
 
+    def __exit__(self, *args):
+        self.disconnect()
 
-def disconnect(skt):
-    '''
-    Closes the connection on the given socket.
+    def __repr__(self):
+        return '<AjpConnection: {0}:{1}>'.format(self._host_name, self._port)
 
-    :param skt: socket to close connection on.
-    '''
-    PROTOCOL_LOGGER.debug('Closing %s', skt)
-    skt.close()
+    def connect(self):
+        self._socket.connect((self._host_name, self._port))
+        PROTOCOL_LOGGER.info('Connected {}'.format(self.__repr__()))
 
+    def disconnect(self):
+        PROTOCOL_LOGGER.debug('Closing connection...')
+        self._socket.close()
 
-def send_and_receive(skt, ajp_request):
-    '''
-    Performs the data exchange with the servlet container.
-
-    :param skt: socket connection to the servlet container.
-    :param ajp_request: request to send to the servlet container.
-
-    :return: the AjpResponse object
-    '''
-    buffer = skt.makefile('rb')
-    request_packet = ajp_request.serialize_to_packet()
-    PROTOCOL_LOGGER.debug(
-        'request_packet sent:\n%s', hexdump(
-            request_packet, result='return'))
-    skt.sendall(request_packet)
-    ajp_resp = AjpResponse.parse(buffer, ajp_request)
-    buffer.close()
-    return ajp_resp
+    def send_and_receive(self, ajp_request):
+        buffer = self._socket.makefile('rb')
+        request_packet = ajp_request.serialize_to_packet()
+        PROTOCOL_LOGGER.debug(
+            'request_packet sent:\n%s', hexdump(
+                request_packet, result='return'))
+        self._socket.sendall(request_packet)
+        ajp_resp = AjpResponse.parse(buffer, ajp_request)
+        buffer.close()
+        return ajp_resp
